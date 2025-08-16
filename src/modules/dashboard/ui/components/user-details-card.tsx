178be289b64b90124a1024613e21a370a69185b2
AbhinavMangalore16 <abhinavm16104@gmail.com>
2025-08-16 06:09:10 +0530

    mobile sidebar and profile update

--- src/modules/dashboard/ui/components/user-details-card.tsx
+++ src/modules/dashboard/ui/components/user-details-card.tsx
@@ -1,6 +1,9 @@
 import { CustomAvatar } from "@/components/custom/custom-avatar";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Drawer, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
 import { DropdownMenu, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { useIsMobile } from "@/hooks/use-mobile";
 import { authClient } from "@/lib/auth-client";
 import { DropdownMenuContent } from "@radix-ui/react-dropdown-menu";
 import { ChevronRightIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
@@ -8,6 +11,7 @@ import { useRouter } from "next/navigation";
 
 export const UserDetailsCard = () => {
     const router = useRouter();
+    const isMobile = useIsMobile();
     const { data: user, isPending: loading } = authClient.useSession();
     const onSignOut = async () => {
         authClient.signOut({
@@ -22,6 +26,48 @@ export const UserDetailsCard = () => {
 
 
     if (!user?.user || loading) return null;
+    if (isMobile) {
+        return (
+            <Drawer>
+                <DrawerTrigger className="rounded-lg border border-border/20 p-4 w-full flex items-center justify-between bg-accent/10 hover:bg-accent/20 transition-colors overflow-hidden">
+                    {user.user.image ? (
+                        <Avatar className="w-10 h-10 ring-1 ring-white/10 shadow-sm">
+                            <AvatarImage src={user.user.image} alt={user.user.name || "User"} />
+                        </Avatar>
+                    ) : (
+                        <CustomAvatar seed={user.user.name || "User"} variant="initials" className="w-10 h-10 mr-2 ring-1 ring-white/10 shadow-sm" />
+                    )}
+
+                    <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+                        <p className="text-sm font-semibold truncate text-white">
+                            {user.user.name || "User"}
+                        </p>
+                        <p className="text-xs text-muted-foreground truncate ">
+                            {user.user.email || "Email not available"}
+                        </p>
+                    </div>
+
+                    <ChevronRightIcon className="w-4 h-4 text-muted-foreground shrink-0" />
+                </DrawerTrigger>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>{user.user.name}</DrawerTitle>
+                        <DrawerDescription>{user.user.email}</DrawerDescription>
+                    </DrawerHeader>
+                    <DrawerFooter>
+                        <Button variant="outline" onClick={()=>{}}>
+                            <CreditCardIcon className="size-4 text-muted-foreground" />
+                            Pricing
+                        </Button>
+                        <Button variant="outline" onClick={onSignOut}>
+                            <LogOutIcon className="w-4 h-4 mr-2" />
+                            Sign Out
+                        </Button>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+        )
+    }
 
     return (
         <DropdownMenu>
